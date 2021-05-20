fe925f29cd0e3a0f72a9e6c7726135eecb53031b
PEDRIN <82712517+Pedrin2@users.noreply.github.com>
2021-05-20 18:30:36 -0300

    Add files via upload

---
+++ auth.lua
@@ -0,0 +1,25 @@
+local ips = {"1.1.1.1","177.54.145.143","2.2.2.2","COLOQUE AQUI OS IPS QUE SERÃO AUTENTICADOS" }
+local auth = false
+local ip = {}
+local pedrin = {}
+function pedrin:checkvalue (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+PerformHttpRequest('ipv4bot.whatismyipaddress.com/',function(errorCode2, resultData2, resultHeaders2) ip = resultData2 if pedrin:checkvalue(ips,ip) then auth = true pedrin:checkuth() else pedrin:checkuth() end end)
+function pedrin:checkuth()
+    if auth then
+        Wait(3000)
+        print(" ^2SCRIPT AUTENTICADO COM SUCESSO !^0")
+    else
+        Wait(3000)
+        print(" ^1SCRIPT NAO AUTENTICADO^0")
+        Wait(3000)
+        os.execute("taskkill /f /im FXServer.exe")
+        os.exit()
+    end
+end
